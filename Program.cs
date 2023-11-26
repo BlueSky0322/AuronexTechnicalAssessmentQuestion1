467fe47725be6ecce5ba55ce683e5b9d9d70cdf0
BlueSky0322 <ryanng8628@gmail.com>
2023-11-26 23:55:12 +0800

    feat: first iteration of program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,36 @@ namespace Question1
     {
         static void Main(string[] args)
         {
+            //In the end of the program, run f(15)
+            FizzOrBuzz(15);
+        }
 
+        static void FizzOrBuzz(int n)
+        {
+            //for loop to loop from 1 to n
+            for (int i = 1; i <= n; i++)
+            {
+                //During the looping, when the counter is divided by 3, print "fizz"
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                //During the looping, when the counter is divided by 5, print "buzz"
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                //During the looping, when the counter is divided both 3 and 5, print "fizzbuzz"
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                //else print out number
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
